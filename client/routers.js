48ab1bfa4f5182f599042711241c3048d10d9e33
pipluploverpunk <pipluploverpunk>
2015-11-07 16:21:53 -0500

    initial commit

---
+++ client/routers.js
@@ -0,0 +1,12 @@
+Router.route('/', {
+    template: 'about',
+    title: 'creativitythroughcode - about'
+});
+Router.route('/gallery',{
+    template: 'gallery',
+    title: 'creativitythroughcode - gallery'
+});
+Router.route('/contact',{
+    template: 'contact',
+    title: 'creativitythroughcode - contact'
+});
